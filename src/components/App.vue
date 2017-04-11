4d31e439a8d90842d574313c56f834ebe913feb3
huyanjie <huyanjie@touguyun.cn>
2017-04-11 19:38:12 +0800

    Init

---
+++ src/components/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+        Index Page
+    </div>
+</template>
+<script>
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'file-loader?name=[name].[ext]?[hash]'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    },
+    devServer: {
+        inline: true,
+        hot: false,
+        grogress: true,
+        historyApiFallback: true,
+    }
+}
